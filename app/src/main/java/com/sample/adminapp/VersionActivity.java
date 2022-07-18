8fe77f7492116ced9da6caae7ff878478ad28068
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-07-18 13:37:52 +0530

    VersionActivity.java

--- app/src/main/java/com/sample/adminapp/VersionActivity.java
+++ app/src/main/java/com/sample/adminapp/VersionActivity.java
@@ -1,8 +1,8 @@
 package com.sample.adminapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class VersionActivity extends AppCompatActivity {
 
@@ -10,7 +10,5 @@ public class VersionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_version);
-
-        
     }
 }
